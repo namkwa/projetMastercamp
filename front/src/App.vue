377b7fc0124e8bd2958bf9f776e600ee03c0dbf9
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-06-18 09:50:39 +0200

    cleaning home page

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-<div>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-</div>
-  
+  <div>
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
 
 <style>
