2270b30d61d6390e21d50f2f3647325a66120a1c
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-06-18 11:05:20 +0200

    create account page

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="nav">
     <router-link to="/">Home</router-link> |
-    <router-link to="/login">Login</router-link>
+    <router-link to="/login">Login</router-link> |
+    <router-link to="/create">Create Account</router-link>
   </div>
   <router-view />
 </template>
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -14,7 +14,12 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/Login.vue')
-  }
+  },
+  {
+    path: '/create',
+    name: 'CreateAcc',
+    component: () => import(/* webpackChunkName: "about" */ '../views/CreateAcc.vue')
+  },
 ]
 
 const router = createRouter({
