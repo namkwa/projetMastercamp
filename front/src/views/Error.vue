5e57b382256c5103d8f8b9c2b68042f7670d3492
Adrien Calmesnil <adcalmesnil@gmail.com>
2021-06-25 09:41:16 +0200

    error page update, done

--- front/src/views/Error.vue
+++ front/src/views/Error.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="error">
     <div class="error_wrapper">
-      <div class="error_wrapper_title"><h1>Oups !</h1></div>
       <div class="error_wrapper_content">
-        Une erreur s'est produite, la page que<br />
-        vous cherchez semble introuvable :(
+        <div class="error_wrapper_content_title"><h1>Oups !</h1></div>
+        <div class="error_wrapper_content_text">
+          Une erreur s'est produite, la page que<br />
+          vous cherchez semble introuvable :(
+        </div>
       </div>
     </div>
   </div>
@@ -23,23 +25,29 @@
 }
 
 .error_wrapper {
-  /* display: inline;
-  justify-content: flex-start;
-  align-items: center; */
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 80vh;
   width: 60vw;
 }
 
-.error_wrapper_title {
+.error_wrapper_content {
+  padding-bottom: 80px;
+}
+
+.error_wrapper_content_title {
   font-size: 3em;
   color: #757575;
   font-weight: bold;
+  padding-bottom: 30px;
 }
 
-.error_wrapper_content {
+.error_wrapper_content_text {
   /* display: flex; */
   font-size: 1.5em;
   color: #757575;
   font-weight: bold;
+  text-align: justify;
 }
 </style>
